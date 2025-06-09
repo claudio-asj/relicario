5e81c377661ea4c69d04548276cd1ccfbf1b2c08
Claudio Junior <claudio@karyon.com.br>
2025-06-09 16:23:09 -0300

    teste

--- Relicário/src/pages/Home.tsx
+++ Relicário/src/pages/Home.tsx
@@ -14,7 +14,7 @@ export function Home() {
         <div className="absolute inset-0 bg-black/40 backdrop-blur-sm" />
 
         <div className="relative z-10 text-center px-4 md:px-6 max-w-3xl">
-          <h1 className="text-4xl font-black text-white md:text-5xl lg:text-6xl font-semibold leading-tight text-foreground tracking-tight drop-shadow-md">
+          <h1 className="text-4xl font-black text-white md:text-5xl lg:text-6xl leading-tight text-foreground tracking-tight drop-shadow-md">
             A Elegância está nos detalhes
           </h1>
 
