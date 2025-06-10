eef01cdb3ec62a7b017633eee452a192999c7591
Claudio Junior <claudio@karyon.com.br>
2025-06-10 12:28:46 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, ReactNode, useEffect, useMemo } from 'react';
+import  { createContext, useState, useContext, ReactNode, useEffect, useMemo } from 'react';
 
 // Definimos o tipo para um item do carrinho
 export type CartItem = {
