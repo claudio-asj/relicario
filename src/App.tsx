4f39e2cd5be40ed7758f54acf724b064766bea01
Claudio Jr <claudio@karyon.com.br>
2025-06-16 16:09:21 -0300

    add ga4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import { CartDialog } from "./components/CartDialog";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { CartProvider } from "./contexts/CartContext";
 import AppRoutes from "./Routes";
+import ReactGA from 'react-ga4';
+const TRACKING_ID = "G-KF3CNYBZMD"; // your Measurement ID
 
 export default function App() {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    // Send pageview with a custom path
+    ReactGA.send({ hitType: "pageview", page: "/", title: "Charme e Desapego - Kelly" });
+}, [])
   return (
     <>
       <CartProvider>
